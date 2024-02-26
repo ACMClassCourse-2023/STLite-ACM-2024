f2542611b0758c88c1844787a0d8c0d77549bf0f
Wankupi <2893353848@qq.com>
2024-02-26 11:07:02 +0800

    fix(vector): bugs of template pair

--- vector/src/utility.hpp
+++ vector/src/utility.hpp
@@ -15,11 +15,11 @@ public:
 	pair(pair &&other) = default;
 	pair(const T1 &x, const T2 &y) : first(x), second(y) {}
 	template<class U1, class U2>
-	pair(U1 &&x, U2 &&y) : first(x), second(y) {}
+	pair(U1 &&x, U2 &&y) : first(std::forward(x)), second(std::forward(y)) {}
 	template<class U1, class U2>
 	pair(const pair<U1, U2> &other) : first(other.first), second(other.second) {}
 	template<class U1, class U2>
-	pair(pair<U1, U2> &&other) : first(other.first), second(other.second) {}
+	pair(pair<U1, U2> &&other) : first(std::move(other.first)), second(std::move(other.second)) {}
 };
 
 }
